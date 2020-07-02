68e8c0f0f8a892f71b9d31c9cc8a4a5a0f6e22fd
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-07-02 23:06:54 +0200

    Improved test and added launch config to debug tests

--- src/components/FormInput/FormInput.test.tsx
+++ src/components/FormInput/FormInput.test.tsx
@@ -1,11 +1,13 @@
+import '@testing-library/jest-dom';
 import { render, configure } from "@testing-library/react";
+import React from 'react';
 import FormInput from './FormInput';
 
 configure({ testIdAttribute: "data-spec" });
 
 describe("Task.vue", () => {
 	it("component renders", () => {
-        const { getByTestId } = render(FormInput);
+        const { getByTestId } = render(<FormInput label="asdf"/>);
         
         getByTestId('formInput');
 
--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -8,8 +8,8 @@ type Props = {
 export default function FormInput(props: Props) {
     return (
         <div className="rcm-form-input">
-            <span className="rcm-form-input__label">{ props.label }</span>
-            <input className="rcm-form-input__input" type="text" />
+            <span data-spec="formLabel" className="rcm-form-input__label">{ props.label }</span>
+            <input data-spec="formInput" className="rcm-form-input__input" type="text" />
         </div>
     )
 }
